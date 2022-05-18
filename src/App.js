eb5359b36a539e9c7993f0f8d34b7196296d590b
asadullashaikh <77007576+asadullashaikh@users.noreply.github.com>
2022-05-18 17:58:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import EnhancedTable from './table'
+import Edit from './edit'
+
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Checkbox, Button, IconButton } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [data1, setData1] = useState([]);
+  const [id, setId] = useState();
+  const [searchtext, setSearchtext] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        setData1(res.data);
+      });
+  }, []);
+
+  console.log("data",data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='md' sx={{mt:5}}>
+      <Typography variant="h3" align="center">Admin UI</Typography>
+      <Router>
+        <Routes>
+          <Route path="/" element={<EnhancedTable rows={data} data1={data1} setRows={setData} setData1={setData1} setId={setId} />} />
+          <Route path="/edit" element={<Edit rows={data} data1={data1} setRows={setData} setData1={setData1} id={id} />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
